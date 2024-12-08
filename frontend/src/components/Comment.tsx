80956f4fd08bf7bf92c5fb3959ec43652b8720f9
Achraf Elhachemy <117669203+AcEl-js@users.noreply.github.com>
2024-12-08 23:31:09 +0100

    Update Comment.tsx delete the show url aleret

--- frontend/src/components/Comment.tsx
+++ frontend/src/components/Comment.tsx
@@ -486,7 +486,7 @@ const isNewComment = () => {
         </div>
       )}
       <div className="py-2">
-        {/* URL Alert */}
+       {/*  {/* URL Alert */}
         {showUrlAlert && (
               <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 mb-2" role="alert">
                 <div className="flex items-center">
@@ -494,7 +494,7 @@ const isNewComment = () => {
                   <p>This comment contains a URL and may have restricted visibility.</p>
                 </div>
               </div>
-            )}
+            )} */}
            
 
         <div className="flex items-start space-x-2">
